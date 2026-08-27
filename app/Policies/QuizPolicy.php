9ec7df223f5b7f9e59a898f823038850032034db
Henry <littleboy13@yahoo.com>
2026-08-27 18:12:26 +0700

    Update policies

--- app/Policies/QuizPolicy.php
+++ app/Policies/QuizPolicy.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Policies;
 
+use Illuminate\Foundation\Auth\User as AuthUser;
 use App\Models\Quiz;
 use Illuminate\Auth\Access\HandlesAuthorization;
-use Illuminate\Foundation\Auth\User as AuthUser;
 
 class QuizPolicy
 {
     use HandlesAuthorization;
-
+    
     public function viewAny(AuthUser $authUser): bool
     {
         return $authUser->can('ViewAny:Quiz');
@@ -71,4 +71,5 @@ class QuizPolicy
     {
         return $authUser->can('Reorder:Quiz');
     }
+
 }
